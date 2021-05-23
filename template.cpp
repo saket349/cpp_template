c10d5f957ee1df54b0694ef04f3eb33ff347396c
Saket Kumar <62437314+saket349@users.noreply.github.com>
2021-05-23 22:07:24 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,18 +7,20 @@ using namespace std;
 #define ull unsigned long long
 #define ld long double
 
-#define pb	push_back
-#define scd(x)	scanf("%d",&x)
-#define scld(x)	scanf("%ld",&x)
+#define pb		push_back
+#define scd(x)		scanf("%d",&x)
+#define scld(x)		scanf("%ld",&x)
 #define sclld(x)	scanf("%lld",&x)
-#define fo(i,n)	for(int i=0;i<n;i++)
-#define foc(i,k,n)	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define modpro(a,b)  ((a%mod)*(b%mod))%mod
+#define fo(i,n)		for(int i=0;i<n;i++)
+#define fof(i,k,n)	for(int i=k;i<n;i++)
+#define fob(i,k,n)	for(int i=n-1;i>=k;i--)
+#define modpro(a,b) ((a%mod)*(b%mod))%mod
+#define modsum(a,b) ((a%mod)+(b%mod))%mod
 //==================================================================================
 const int mod = 1000000007;
 //==================================================================================
 //-----------------------FUNCTIONS-------------------------------------------------------------------------------------------------------
-//---Returns Pow(x,n)---------
+//---Returns x^n ---------
 ll power(int x,int n)
 {
   int m = abs(n);
@@ -26,7 +28,7 @@ ll power(int x,int n)
     while(m)
     {
         if(m%2==0)
-        x=x*2;
+        x=x*x;
         else
         {
             ans=ans*x;
@@ -36,58 +38,26 @@ ll power(int x,int n)
     }
     return ans;
 }
-// Retruns modularExponentiation
-ll modularExponentiation(ll x,ll n,ll M)
+
+//---Returns modular exponential---------
+ll Mpower(int x,int n)
 {
-    ll result=1;
-    while(n>0)
+    int m = abs(n);
+    ll ans=1;
+    while(m)
     {
-        if(n % 2 ==1)
-            result=modpro(result,x);
+        if(m%2==0)
         x=modpro(x,x);
-        n=n/2;
+        else
+        {
+            ans=modpro(ans,x);
+            x=modpro(x,x);
+        }
+    	m=m/2;
     }
-    return result;
+    return ans;
 }
 
-// Returns factorial of n --------
-ll fact(int n) 
-{ 
-    ll res = 1; 
-    for (int i = 2; i <= n; i++) 
-        res = res * i; 
-    return res; 
-} 
-
-//Returns nCr--------------------
-ll nCr(int n, int r) 
-{ 
-    return fact(n) / (fact(r) * fact(n - r)); 
-}
-// Returns n^(-1) mod p 
-unsigned long long modInverse(unsigned long long n, int p) 
-{ 
-    return modularExponentiation(n, p - 2, p); 
-}
-
-// Return nCr%p using Fermant's little theorem
-unsigned long long nCrModPFermat(unsigned long long n, 
-                                 int r, int p) 
-{ 
-    // Base case 
-    if (r == 0) 
-        return 1; 
-  
-    // Fill factorial array so that we 
-    // can find all factorial of r, n 
-    // and n-r 
-    unsigned long long fac[n + 1]; 
-    fac[0] = 1; 
-    for (int i = 1; i <= n; i++) 
-        fac[i] = (fac[i - 1] * i) % p; 
-  
-    return (fac[n] * modInverse(fac[r], p) % p * modInverse(fac[n - r], p) % p) % p; 
-} 
 //Return GCD
 int gcd(int a, int b)
 {
@@ -104,6 +74,7 @@ int lcm(int a, int b)
 //__gcd(a,b)  to calculate gcd of two numbers
 
 
+
 int main()
 {
 ios_base::sync_with_stdio(0); 
@@ -112,7 +83,7 @@ cin.tie(0); cout.tie(0);
     cin>>t;
     while(t--)
     {
-        
+     
     }
 return 0;
 }
