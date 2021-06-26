90907fbc5230bdaeed8e287f6c00be4665d9d334
Saket Kumar Singh <62437314+saket349@users.noreply.github.com>
2021-06-26 12:09:20 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,6 +6,11 @@ using namespace std;
 #define ll long long 
 #define ull unsigned long long
 #define ld long double
+#define pi pair<int,int>
+#define pl pair<long long, long long>
+#define mp make_pair
+#define f first
+#define s second
 
 #define pb		push_back
 #define scd(x)		scanf("%d",&x)
@@ -14,8 +19,9 @@ using namespace std;
 #define fo(i,n)		for(int i=0;i<n;i++)
 #define fof(i,k,n)	for(int i=k;i<n;i++)
 #define fob(i,k,n)	for(int i=n-1;i>=k;i--)
-#define modpro(a,b) ((a%mod)*(b%mod))%mod
-#define modsum(a,b) ((a%mod)+(b%mod))%mod
+#define modpro(a,b) 	((a%mod)*(b%mod))%mod
+#define modsum(a,b) 	((a%mod)+(b%mod))%mod
+#define no	        cout<<"NO\n"
 //==================================================================================
 const int mod = 1000000007;
 //==================================================================================
@@ -40,22 +46,17 @@ ll power(int x,int n)
 }
 
 //---Returns modular exponential---------
-ll Mpower(int x,int n)
+ll modularExponentiation(ll x,ll n,ll M)
 {
-    int m = abs(n);
-    ll ans=1;
-    while(m)
+    ll result=1;
+    while(n>0)
     {
-        if(m%2==0)
+        if(n % 2 ==1)
+            result=modpro(result,x);
         x=modpro(x,x);
-        else
-        {
-            ans=modpro(ans,x);
-            x=modpro(x,x);
-        }
-    	m=m/2;
+        n=n/2;
     }
-    return ans;
+    return result;
 }
 
 //Return GCD
@@ -70,16 +71,51 @@ int lcm(int a, int b)
 {
     return (a / gcd(a, b)) * b;
 }
+// Returns modulo inverse of a with respect
+// to m using extended Euclid Algorithm
+// Assumption: a and m are coprimes, i.e.,
+// gcd(a, m) = 1
+int modInverse(int a, int m)
+{
+	int m0 = m, y = 0, x = 1;
+	if (m == 1)
+		return 0;
+	while (a > 1) {
+		// q is quotient
+		int q = a / m;
+		int t = m;
+		// m is remainder now, process same as
+		// Euclid's algo
+		m = a % m, a = t;
+		t = y;
+		// Update y and x
+		y = x - q * y;
+		x = t;
+	}
+	// Make x positive
+	if (x < 0)
+		x += m0;
+	return x;
+}
+// asc sort
+bool sortcol( const vector<ll>& v1,
+               const vector<ll>& v2 ) {
+                  
+ return v1 < v2;
+}
 //----------------------------------------------------------------------------------------------------------------------------------------
 //__gcd(a,b)  to calculate gcd of two numbers
-
+/*
+	priority_queue <T, vector<T>, greater<T>> pq; // 30    20    10    5    1
+	
+*/
 
 
 int main()
 {
 ios_base::sync_with_stdio(0); 
 cin.tie(0); cout.tie(0);
-    int t;
+    int t = 1;
     cin>>t;
     while(t--)
     {
